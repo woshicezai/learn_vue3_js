9d548ffb4d7798cce26d437806726a951632e8e6
woshicezai <behappys@sina.com>
2023-05-26 22:09:14 +0800

    feat: 添加卸载函数&事件绑定

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -32,17 +32,42 @@ function createRenderer({ createElement, insert, setElementText, patchProps }) {
    * @param {*} container
    */
   function patch(oldNode, newNode, container) {
-    if (!oldNode) {
-      //意味着挂载
-      mountElement(newNode, container);
+    //如果新旧节点的类型不一样，则先将旧节点直接卸载
+    if (oldNode && oldNode.type !== newNode.type) {
+      unmount(oldNode);
+      oldNode = null;
+    }
+    const { type } = newNode;
+    if (typeof type === "string") {
+      if (!oldNode) {
+        //意味着挂载
+        mountElement(newNode, container);
+      } else {
+        //打补丁
+      }
+    } else if (typeof type === "object") {
+      //自定义组件
     } else {
-      //打补丁
+      //其他类型的了，比如 fragment
+    }
+  }
+
+  /**
+   * 卸载
+   * @param {*} vnode
+   */
+  function unmount(vnode) {
+    //卸载
+    const el = vnode.el;
+    const parent = el.parentNode;
+    if (parent) {
+      parent.removeChild(el);
     }
   }
 
   //挂载
   function mountElement(vnode, container) {
-    const el = createElement(vnode.type);
+    const el = (vnode.el = createElement(vnode.type));
     //处理children
     if (typeof vnode.children === "string") {
       setElementText(el, vnode.children);
@@ -65,8 +90,7 @@ function createRenderer({ createElement, insert, setElementText, patchProps }) {
     if (vnode) {
       patch(container._vnode, vnode, container);
     } else if (container._vnode) {
-      //卸载
-      container.innerHTML = "";
+      unmount(container._vnode);
     }
     container._vnode = vnode;
   }
@@ -100,6 +124,28 @@ const { render } = createRenderer({
   insert: (el, parent, anchor = null) => parent.insertBefore(el, anchor),
   setElementText: (el, text) => (el.textContent = text),
   patchProps: (el, key, preValue, nextValue) => {
+    if (/^on/.test(key)) {
+      //以on开头的属性，认为是事件
+      const name = key.slice(2).toLowerCase();
+      const invokers = el._vei || (el._vei = {});
+      let invoker = invokers[name];
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei[name] = (e) => {
+            //一个元素可以多个事件，一个事件可以多个回调绑定
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach((inv) => inv(e));
+            } else {
+              invoker.value(e);
+            }
+          };
+          el.addEventListener(name, invoker);
+        }
+        invoker.value = nextValue;
+      } else if (invoker) {
+        el.removeEventListener(name, invoker);
+      }
+    }
     //优先对class做特殊处理
     if (key === "class") {
       el.className = nextValue || "";
